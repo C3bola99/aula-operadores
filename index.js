9726571a557dcf41d9a21cda267fa10db38cd842
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-19 16:50:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//exercício 1
+
+let idade = prompt( "qual a sua idade:" )
+let idadeAmigo= prompt( "qual a idade do seu amigo(a)? " )
+
+console.log ("Sua idade é maior do que a do seu melhor amigo?" , idade>idadeAmigo)
+console.log ("A diferença de idade entre os dois é de:" , idade>idadeAmigo)
+
+
+//fim exercício1
+
+//exercício 2
+
+let N1 = prompt( "Digite um número:" )
+const resultado = (Number(N1) %2 )
+
+console.log ("o resto da divisão desse número é de:" , resultado)
+//o restante da divisão de número sempre retorna 0
+//o restando da divisão de número ímpar sempre retorna 1
+
+//fim do exercício 2
+
+
+//exercício 3
+
+let idade = prompt( "qual a sua idade? " )
+
+
+console.log("Olá " + nome + ", você tem " + idade + " anos.");
