80f37e6ef0b281b6b183ccb2e2fb244f9663c217
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-23 16:55:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,67 @@
+//let raçaDeCusco = ['Salsichinha','Yorkshire','Shitzu','Poodle','Rottwailer']
+//let frase = prompt("insira um número de 0 a 4")
+//frase = Number(frase)
+//console.log("a raça escolhida foi:", raçaDeCusco[frase] )
+
+
+//let meuArray = [1, 2, 3, 4, 5, 6];
+//let tamanhoOriginal = meuArray.length;
+//console.log("Tamanho original do array:", tamanhoOriginal);
+//meuArray.push(7);
+//meuArray.splice(3, 2);
+//let novoTamanho = meuArray.length;
+//console.log("Novo tamanho do array:", novoTamanho);
+//console.log(meuArray);
+
+//Exercícios de interpretação
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+//a- nao retorna nada
+//b- retorna nulo
+//c- retorna 11(.LENGHT)
+//d- retorna 3
+//e- retorna uma array(11)
+//f- retorna 9
+
+//Exercícios de interpretação 2
+
+//const frase = prompt("Digite uma frase")
+
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Qual será o valor impresso no console se a entrada do usuário for:  "Subi num ônibus em Marrocos"?
+
+//o valor impresso será de: SUBI NUM ONIBUS EM MIRROCOS (LENGHT(27))
+
+
+
+//Exercícios de escrita de código
 //exercício 1
 
-let idade = prompt( "qual a sua idade:" )
-let idadeAmigo= prompt( "qual a idade do seu amigo(a)? " )
+//let nome = prompt( " digite seu nome: " )
+//let email = prompt( " qual seu email? " )
+//console.log ("o Email" , email , " foi cadastrado com sucesso, seja bem vindo" , nome )
 
-console.log ("Sua idade é maior do que a do seu melhor amigo?" , idade>idadeAmigo)
-console.log ("A diferença de idade entre os dois é de:" , idade>idadeAmigo)
-
-
-//fim exercício1
 
 //exercício 2
 
-let N1 = prompt( "Digite um número:" )
-const resultado = (Number(N1) %2 )
+let comidas = ['Lasanha','Panqueca','Pizza','Shoarma','Temaki']
+console.log(comidas)
+console.log("essas são minhas comidas preferidas")
 
-console.log ("o resto da divisão desse número é de:" , resultado)
-//o restante da divisão de número sempre retorna 0
-//o restando da divisão de número ímpar sempre retorna 1
-
-//fim do exercício 2
-
-
-//exercício 3
-
-// Pergunta a idade do usuário em anos
-let idadeAnos = prompt("Qual é a sua idade em anos?");
-
-// Converte a idade para números inteiros
-idadeAnos = parseInt(idadeAnos);
-
-// Calcula a idade em meses, dias e horas
-let idadeMeses = idadeAnos * 12;
-let idadeDias = idadeAnos * 365; // Considerando que um ano tem 365 dias
-let idadeHoras = idadeDias * 24;
-
-// Imprime as informações no console
-console.log("Sua idade em meses é: " + idadeMeses);
-console.log("Sua idade em dias é: " + idadeDias);
-console.log("Sua idade em horas é: " + idadeHoras);
-
-//fim do exercício 3
-
-
-//exercício 4
-
-// Pergunta ao usuário dois números
-let numero1 = prompt("Digite o primeiro número:");
-let numero2 = prompt("Digite o segundo número:");
-
-// Converte os números para tipo numérico
-numero1 = parseInt(numero1);
-numero2 = parseInt(numero2);
-
-// Verifica se o primeiro número é maior que o segundo
-let maiorQue = numero1 > numero2;
-
-// Verifica se o primeiro número é igual ao segundo
-let igualA = numero1 === numero2;
-
-// Verifica se o primeiro número é divisível pelo segundo
-let divisivelPor = numero1 % numero2 === 0;
-
-// Verifica se o segundo número é divisível pelo primeiro
-let divisivelPor2 = numero2 % numero1 === 0;
-
-// Imprime os resultados
-console.log("O primeiro número é maior que o segundo? " + maiorQue);
-console.log("O primeiro número é igual ao segundo? " + igualA);
-console.log("O primeiro número é divisível pelo segundo? " + divisivelPor);
-console.log("O segundo número é divisível pelo primeiro? " + divisivelPor2);
-
-
-//escrevendo em diferentes strings
-
-let nome = prompt( "qual seu nome?" )
-let corFavorita= prompt( "qual sua cor favorita? " )
-let frase = `a cor preferida de ${nome} é ${corFavorita}`
-console.log (frase)
